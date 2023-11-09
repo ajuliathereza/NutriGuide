0c03f01914d9104fc2fa46abbcd28bf78268c421
ajuliathereza <m00nlh3r@gmail.com>
2023-11-09 20:21:56 -0300

    edit 1.4

--- src/script.js
+++ src/script.js
@@ -25,28 +25,43 @@ function go(currentStep, nextStep) {
     dBlock.style.display = 'block';
 }
 
-function calculateProteina() {
+function calculateMacroConsumption() {
     const peso = document.getElementById('peso');
     const altura = document.getElementById('altura');
+    const idade = document.getElementById('idade');
     peso.style.border = 'none';
     altura.style.border = 'none';
+    idade.style.border = 'none';
 
-    if (!peso.value || !altura.value) {
-        if (!peso.value && !altura.value) {
+    if (!peso.value || !altura.value || !idade.value) {
+        if (!peso.value) {
             peso.style.border = '1px solid red';
+        }
+        if (!altura.value) {
             altura.style.border = '1px solid red';
-        } else if (!peso.value) {
-            peso.style.border = '1px solid red';
-        } else {
-            altura.style.border = '1px solid red';
+        }
+        if (!idade.value) {
+            idade.style.border = '1px solid red';
         }
     } else {
         const pesoCorporal = parseFloat(peso.value);
+        const alturaCorporal = parseFloat(altura.value);
+        const idadePessoa = parseInt(idade.value);
 
-        // Cálculo da quantidade recomendada de proteína
+        // Cálculo da quantidade recomendada de proteína (sem considerar a idade)
         const quantidadeProteina = 2 * pesoCorporal;
+
+        // Cálculo da quantidade recomendada de carboidratos (3x peso corporal)
+        const quantidadeCarboidratos = 3 * pesoCorporal;
+
+        // Cálculo da quantidade recomendada de gordura (1x peso corporal)
+        const quantidadeGordura = pesoCorporal;
+
+        // Cálculo do consumo diário de água (35x peso corporal)
+        const consumoAgua = 35 * pesoCorporal;
+        
         const result = document.getElementById('resultado');
-        result.innerHTML = 'Proteína recomendada: ' + quantidadeProteina + ' gramas';
+        result.innerHTML = `Proteína: ${quantidadeProteina.toFixed(2)} gramas<br>Carboidratos: ${quantidadeCarboidratos.toFixed(2)} gramas<br>Gordura: ${quantidadeGordura.toFixed(2)} gramas<br>Consumo de água: ${consumoAgua.toFixed(2)} litros `;
         go(2, 3);
     }
 }
